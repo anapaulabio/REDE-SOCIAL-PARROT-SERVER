9f17636247cdec6bece78f7da13f834a3feca4ef
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-10-23 19:10:51 -0300

    create user route

---
+++ src/adapter/apis/routes/users.routes.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { CommonRoutesConfig } from "./common.routes.config";
+import usersController from '../controllers/users.controller';
+
+
+export class UsersRoutes extends CommonRoutesConfig {
+    constructor(app: express.Application){
+      super(app, 'UsersRoutes')
+    }
+
+    configureRoutes(): express.Application {
+        this.app.route('/users')
+            .get(usersController.getUsers)
+        
+        this.app.route('/cadastro')
+            .post(usersController.createUsers)
+        
+       // this.app.route('/login')
+       //     .post(authHelperAdapter.login)
+    
+        this.app.route('/users/:UserId')
+            .get(usersController.getUsersById)
+            .put(usersController.updateUsers)
+            .delete(usersController.removeUsers)
+            
+    
+        return this.app
+    } 
+}
+

3094fe4368fddbda28223901c1401bb0e975be16
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-10-24 09:27:29 -0300

    setting login in files

--- src/adapter/apis/controllers/users.controller.ts
+++ src/adapter/apis/controllers/users.controller.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import debug from 'debug';
 
-import createUserUsecase from '../../../domain/usecases/user/create.user.usecase';
-import listUserUsecase from '../../../domain/usecases/user/list.user.usecase';
-import updateUserUsecase from '../../../domain/usecases/user/update.user.usecase';
-import deleteUserUsecase from '../../../domain/usecases/user/delete.user.usecase';
-import readUserUsecase from '../../../domain/usecases/user/read.user.usecase';
+import createUserUsecase from '../../../domain/usecases/users/create.user.usecase';
+import listUserUsecase from '../../../domain/usecases/users/list.user.usecase';
+import updateUserUsecase from '../../../domain/usecases/users/update.user.usecase';
+import deleteUserUsecase from '../../../domain/usecases/users/delete.user.usecase';
+import readUserUsecase from '../../../domain/usecases/users/read.user.usecase';
+import loginUserUsecase from '../../../domain/usecases/users/login.user.usecase';
+import { getErrorMessage } from '../../helpers/errors.helper.adapter';
 
 const log: debug.Debugger = debug('app: users-controller')
 
@@ -46,6 +48,15 @@ class UsersController {
         })
         res.status(204).send()
     }
+
+    async loginOne(req: express.Request, res: express.Response){
+        try {
+          const foundUser = await loginUserUsecase.execute(req.body);
+          res.status(200).send(foundUser);
+        } catch (error) {
+          return res.status(500).send(getErrorMessage(error));
+        }
+       };
 }
 
 export default new UsersController();

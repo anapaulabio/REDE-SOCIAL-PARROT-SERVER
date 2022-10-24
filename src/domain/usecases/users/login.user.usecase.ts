a589f7a14d3094805768fc276e06c9c0e21f0112
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-10-24 09:26:23 -0300

    create login use case

---
+++ src/domain/usecases/users/login.user.usecase.ts
@@ -0,0 +1,14 @@
+import { IUseCase } from "../interface.usecase";
+import { IUsersEntity } from "../../entities/users.entity";
+import { IUsersRepository } from "../../repositories/users.repository.interface";
+import usersRepository from "../../../adapter/repositories/users.repository";
+
+class LoginUseCase implements IUseCase {
+    constructor(private _repository: IUsersRepository){}
+
+    async execute(data: {email: string, password: string}): Promise<IUsersEntity> {
+        return await this._repository.login(data)
+    }
+}
+
+export default new LoginUseCase(usersRepository)

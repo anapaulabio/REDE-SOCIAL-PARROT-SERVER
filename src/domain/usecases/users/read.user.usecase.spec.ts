02ea6e8cf0a7ff324153c352b461a59ab6e0bd6c
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-10-26 18:47:41 -0300

    creat read test

---
+++ src/domain/usecases/users/read.user.usecase.spec.ts
@@ -0,0 +1,23 @@
+import readUserUsecase from "./read.user.usecase";
+import createUserUsecase from "./create.user.usecase";
+import { IUsersEntity } from "../../entities/users.entity";
+
+
+test("Teste unitário de readUserUseCase", async() => {
+    const user: IUsersEntity = {
+		"name": "Venus",
+		"email": "joao@exemplo.com",
+		"apartment": 40,
+		"password": "$2b$10$4cYhvynIHe/GfTFIpjaDquCscIwoyezoRTOcRgFAFsGpmgr7cg2oC",
+		"linkdafoto": "linkdafoto.jpeg"
+    };
+    await createUserUsecase.execute(user)
+    const user2: IUsersEntity = {
+		"name": "Venus",
+		"email": "joao@exemplo.com",
+		"apartment": 40,
+		"password": "$2b$10$4cYhvynIHe/GfTFIpjaDquCscIwoyezoRTOcRgFAFsGpmgr7cg2oC",
+		"linkdafoto": "linkdafoto.jpeg"
+    };
+    expect(await readUserUsecase.execute({UserId: 0})).toContain(user2)
+})

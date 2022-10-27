aafac0e52ef4eb68115395c09a3473f0c1ebcad6
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-10-27 16:44:20 -0300

    review mocks in post and users

--- src/infrastructure/persistence/mocks/users/mocks.interface.ts
+++ src/infrastructure/persistence/mocks/users/mocks.interface.ts
@@ -1,5 +1,7 @@
+import { IPostsEntity } from "../../../../domain/entities/post.entity";
 import { IUsersEntity } from "../../../../domain/entities/users.entity";
 
-export default interface IMocksUser {
+export default interface IMocks {
     getUsers(): IUsersEntity[];
+    getPosts(): IPostsEntity[];
 } 
